00df2249239f55c04bcb9479ed1c68b0b2e6378d
abdulbari149 <abdulbari131103@gmail.com>
2020-09-13 19:24:15 +0500

    heade almost completely

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const headerDropDown = [...document.querySelectorAll("[data-fadeBottom]")];
-console.log(headerDropDown);
-headerDropDown.forEach((dropdown) => {
-  let topValue = parseInt(window
-    .getComputedStyle(dropdown, null)
-    .getPropertyValue("top"));
-
-});
