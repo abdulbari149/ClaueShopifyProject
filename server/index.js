f8003775f8fdfef41063316a21cef126bf9c8f40
abdulbari149 <abdulbari131103@gmail.com>
2020-09-19 14:08:40 +0500

    Setting up the server

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+const hostname = '127.0.0.1';
+const port = 3000;
+const webUrl = "../claue/index.html";
+
+const E_commerceServer = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile(webUrl, (error, data)=>{
+      if(error){
+          res.statusCode = 404;
+          res.write("Error: File not found.")
+      }
+      else{
+          res.write(data)
+      }
+      res.end();
+    })
+})
+
+E_commerceServer.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello, World!\n');
+// });
+
+// 
